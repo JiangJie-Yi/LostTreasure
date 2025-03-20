20092b3e3ab0a8119c6420cd76f2b66104d74ae3
je <august49822031@gmail.com>
2025-03-20 12:59:25 +0800

    Progress update: material update

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,7 @@ function App() {
       previousMousePosition.current = { x: event.clientX, y: event.clientY };
     }
   };
+
   const handleMouseUp = () => {
     isDragging.current = false;
     setIsGrabbing(false);
@@ -44,6 +45,11 @@ function App() {
 
   return (
     <>
+      <div className='fixed top-1/10 left-0 w-full h-auto tracking-wide z-10'>
+        <p className='text-zinc-700 text-center font-mono italic'>
+          “ 一封來自共濟會斑駁陳舊的信件 ”
+        </p>
+      </div>
       <Canvas
         className={`z-0 ${isGrabbing ? 'cursor-grabbing' : 'cursor-grab'}`}
         onMouseDown={handleMouseDown}
@@ -51,14 +57,27 @@ function App() {
         onMouseUp={handleMouseUp}
         shadows
       >
-        <ambientLight intensity={0.6} />
-        <pointLight position={[10, 10, 10]} intensity={0.225} castShadow />
-        <directionalLight position={[-5, 5, 5]} intensity={0.225} castShadow />
-        <spotLight position={[0, 5, 5]} angle={0.3} intensity={1} castShadow />
-        <Card position={[0, 1, 0]} rotationAngle={rotationAngle} text={inputText} />
+        <ambientLight intensity={0.15} color={0xffffff} />
+        <pointLight
+          position={[0, -1.25, -2]}
+          intensity={2}
+          penumbra={5}
+          color={0xaef0fc}
+          opacity={0.5}
+          castShadow
+        />
+        <directionalLight position={[0, 10, 0]} intensity={1} color={0xfff6e8} castShadow />
+        <spotLight
+          position={[0, 6, 5]}
+          angle={0.32}
+          intensity={45}
+          penumbra={0.625}
+          target-position={[0, 0, 0]}
+          castShadow
+        />
+        <Card position={[0, 0, 0]} rotationAngle={rotationAngle} text={inputText} />
       </Canvas>
-      <img src={Cipher} />
-      <div className='fixed top-4/6 left-0 w-full h-auto flex justify-self-center items-center flex-col gap-y-1.5 z-10'>
+      <div className='fixed top-7/8 left-0 w-full h-auto flex justify-self-center items-center flex-col gap-y-1.5 z-10'>
         <p className='font-Pigpen-Regular text-amber-300 select-none'>Please enter text</p>
         <Box
           sx={{
@@ -73,18 +92,27 @@ function App() {
             onChange={handleInputChange}
             sx={{
               width: 'fit-content',
-              backgroundColor: 'hsla(0deg, 0%, 100%, 10%)',
+              backgroundColor: 'hsla(0deg, 0%, 100%, 5%)',
               color: 'white',
               borderRadius: '5px',
               outline: 'none',
+              '&:hover': {
+                backgroundColor: 'hsla(0deg, 0%, 100%, 10%)',
+              },
             }}
           />
           <HelpOutlineIcon
             sx={{
               position: 'fixed',
               transform: 'translate(500%, 0)',
-              color: 'white',
+              color: 'hsla(0, 0%, 100%, 0.5)',
               cursor: 'pointer',
+              '&:hover': {
+                color: 'hsla(0, 0%, 100%, 1)',
+              },
+              '&:active': {
+                color: 'hsla(0, 0%, 100%, 0.325)',
+              },
             }}
           />
         </Box>
--- src/card.jsx
+++ src/card.jsx
@@ -3,19 +3,21 @@ import { TextureLoader, ClampToEdgeWrapping, DoubleSide } from 'three';
 import { useFrame, useLoader } from '@react-three/fiber';
 import { Text } from '@react-three/drei';
 import PaperMaterial from '../src/img/paperMaterial.png';
+import Cipher from '../src/img/cipher.png';
 import './css/origin.css';
 
 function Card({ position, rotationAngle, text }) {
   const meshRef = useRef();
-  const texture = useLoader(TextureLoader, PaperMaterial);
+  const mapTexture = useLoader(TextureLoader, PaperMaterial);
+  const textTexture = useLoader(TextureLoader, Cipher);
 
-  texture.wrapS = ClampToEdgeWrapping;
-  texture.wrapT = ClampToEdgeWrapping;
+  mapTexture.wrapS = ClampToEdgeWrapping;
+  mapTexture.wrapT = ClampToEdgeWrapping;
 
-  texture.offset.set(0, 0);
-  texture.repeat.set(1, 1);
+  mapTexture.offset.set(0, 0);
+  mapTexture.repeat.set(1, 1);
 
-  texture.wrapS = texture.wrapT = ClampToEdgeWrapping;
+  mapTexture.wrapS = mapTexture.wrapT = ClampToEdgeWrapping;
 
   useFrame(() => {
     if (meshRef.current) {
@@ -26,10 +28,15 @@ function Card({ position, rotationAngle, text }) {
   });
 
   return (
-    <mesh position={position} ref={meshRef} scale={[1, 1, 0.05]}>
-      <planeGeometry args={[2.5, 3]} />
+    <mesh
+      position={position}
+      ref={meshRef}
+      scale={[1, 1, 0.05]}
+      rotation={[Math.PI / 4, Math.PI / 4, 0]}
+    >
+      <planeGeometry args={[3, 4.125]} />
       <meshPhongMaterial
-        map={texture}
+        map={mapTexture}
         transparent={true}
         alphaTest={0.5}
         depthWrite={true}
@@ -37,14 +44,18 @@ function Card({ position, rotationAngle, text }) {
         shininess={0}
         side={DoubleSide}
       />
+      <mesh position={[0, 0, 0.01]}>
+        <planeGeometry args={[3, 3.5]} />
+        <meshBasicMaterial map={textTexture} transparent={true} opacity={0.6} side={DoubleSide} />
+      </mesh>
       <Text
-        position={[0, 0.25, 0.001]}
+        position={[-1.25, 2, 0.01]}
         fontSize={0.4}
-        maxWidth={0.1}
-        lineHeight={0.25}
-        color='hsl(0, 0%, 0%)'
-        anchorX='center'
-        anchorY='center'
+        maxWidth={1}
+        lineHeight={1.2}
+        color='hsla(0, 0%, 0%, 0.8)'
+        anchorX='left'
+        anchorY='top'
         font='./src/font/Pigpen-Regular.ttf'
       >
         {text}
